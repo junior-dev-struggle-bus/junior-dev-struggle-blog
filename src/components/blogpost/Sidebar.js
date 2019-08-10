21fd704389d755ad12d36af67427495f90374c5f
Brett Hurst <bretthurst@outlook.com>
2019-08-09 19:40:29 -0700

    Removed body tag from Post component, renamed class attr to className in Post and Sidebar

--- src/components/blogpost/Sidebar.js
+++ src/components/blogpost/Sidebar.js
@@ -2,98 +2,100 @@ import React from "react"
 import { Link } from "gatsby"
 
 class Sidebar extends React.Component {
-  render() {
-    return(
-<div id="sidebar" class="col-4 col-12-medium">
-    
-{/* <!-- Excerpts --> */}
-    <section>
-        <ul class="divided">
-            <li>
+    render() {
+        return (
+            <div id="sidebar" className="col-4 col-12-medium">
 
-                {/* <!-- Excerpt --> */}
-                    <article class="box excerpt">
-                        <header>
-                            <span class="date">July 30</span>
-                            <h3><a href="#">Just another post</a></h3>
-                        </header>
-                        <p>Lorem ipsum dolor odio facilisis convallis. Etiam non nunc vel est
+                {/* <!-- Excerpts --> */}
+                <section>
+                    <ul className="divided">
+                        <li>
+
+                            {/* <!-- Excerpt --> */}
+                            <article className="box excerpt">
+                                <header>
+                                    <span className="date">July 30</span>
+                                    <h3><a href="#">Just another post</a></h3>
+                                </header>
+                                <p>Lorem ipsum dolor odio facilisis convallis. Etiam non nunc vel est
                         suscipit convallis non id orci lorem ipsum sed magna consequat feugiat lorem dolore.</p>
-                    </article>
+                            </article>
 
-            </li>
-            <li>
+                        </li>
+                        <li>
 
-                {/* <!-- Excerpt --> */}
-                    <article class="box excerpt">
-                        <header>
-                            <span class="date">July 28</span>
-                            <h3><a href="#">And another post</a></h3>
-                        </header>
-                        <p>Lorem ipsum dolor odio facilisis convallis. Etiam non nunc vel est
+                            {/* <!-- Excerpt --> */}
+                            <article className="box excerpt">
+                                <header>
+                                    <span className="date">July 28</span>
+                                    <h3><a href="#">And another post</a></h3>
+                                </header>
+                                <p>Lorem ipsum dolor odio facilisis convallis. Etiam non nunc vel est
                         suscipit convallis non id orci lorem ipsum sed magna consequat feugiat lorem dolore.</p>
-                    </article>
+                            </article>
 
-            </li>
-            <li>
+                        </li>
+                        <li>
 
-                {/* <!-- Excerpt --> */}
-                    <article class="box excerpt">
-                        <header>
-                            <span class="date">July 24</span>
-                            <h3><a href="#">One more post</a></h3>
-                        </header>
-                        <p>Lorem ipsum dolor odio facilisis convallis. Etiam non nunc vel est
+                            {/* <!-- Excerpt --> */}
+                            <article className="box excerpt">
+                                <header>
+                                    <span className="date">July 24</span>
+                                    <h3><a href="#">One more post</a></h3>
+                                </header>
+                                <p>Lorem ipsum dolor odio facilisis convallis. Etiam non nunc vel est
                         suscipit convallis non id orci lorem ipsum sed magna consequat feugiat lorem dolore.</p>
-                    </article>
+                            </article>
 
-            </li>
-        </ul>
-    </section>
+                        </li>
+                    </ul>
+                </section>
 
-{/* <!-- Highlights --> */}
-    <section>
-        <ul class="divided">
-            <li>
+                {/* <!-- Highlights --> */}
+                <section>
+                    <ul className="divided">
+                        <li>
 
-                {/* <!-- Highlight --> */}
-                    <article class="box highlight">
-                        <header>
-                            <h3><a href="#">Something of note</a></h3>
-                        </header>
-                        <a href="#" class="image left"><img src="images/pic06.jpg" alt="" /></a>
-                        <p>Phasellus sed laoreet massa id justo mattis pharetra. Fusce suscipit ligula vel quam
-                        viverra sit amet mollis tortor congue magna lorem ipsum dolor et quisque ut odio facilisis
-                        convallis. Etiam non nunc vel est suscipit convallis non id orci. Ut interdum tempus
+                            {/* <!-- Highlight --> */}
+                            <article className="box highlight">
+                                <header>
+                                    <h3><a href="#">Something of note</a></h3>
+                                </header>
+                                <a href="#" className="image left"><img src="images/pic06.jpg" alt="" /></a>
+                                <p>Phasellus sed laoreet massa id justo mattis pharetra. Fusce suscipit ligula vel quam
+                                viverra sit amet mollis tortor congue magna lorem ipsum dolor et quisque ut odio facilisis
+                                convallis. Etiam non nunc vel est suscipit convallis non id orci. Ut interdum tempus
                         facilisis convallis. Etiam non nunc vel est suscipit convallis non id orci.</p>
-                        <ul class="actions">
-                            <li><a href="#" class="button icon solid fa-file">Learn More</a></li>
-                        </ul>
-                    </article>
+                                <ul className="actions">
+                                    <li><a href="#" className="button icon solid fa-file">Learn More</a></li>
+                                </ul>
+                            </article>
 
-            </li>
-            <li>
+                        </li>
+                        <li>
 
-                {/* <!-- Highlight --> */}
-                    <article class="box highlight">
-                        <header>
-                            <h3><a href="#">Something of less note</a></h3>
-                        </header>
-                        <a href="#" class="image left"><img src="images/pic07.jpg" alt="" /></a>
-                        <p>Phasellus sed laoreet massa id justo mattis pharetra. Fusce suscipit ligula vel quam
-                        viverra sit amet mollis tortor congue magna lorem ipsum dolor et quisque ut odio facilisis
-                        convallis. Etiam non nunc vel est suscipit convallis non id orci. Ut interdum tempus
+                            {/* <!-- Highlight --> */}
+                            <article className="box highlight">
+                                <header>
+                                    <h3><a href="#">Something of less note</a></h3>
+                                </header>
+                                <a href="#" className="image left"><img src="images/pic07.jpg" alt="" /></a>
+                                <p>Phasellus sed laoreet massa id justo mattis pharetra. Fusce suscipit ligula vel quam
+                                viverra sit amet mollis tortor congue magna lorem ipsum dolor et quisque ut odio facilisis
+                                convallis. Etiam non nunc vel est suscipit convallis non id orci. Ut interdum tempus
                         facilisis convallis. Etiam non nunc vel est suscipit convallis non id orci.</p>
-                        <ul class="actions">
-                            <li><a href="#" class="button icon solid fa-file">Learn More</a></li>
-                        </ul>
-                    </article>
+                                <ul className="actions">
+                                    <li><a href="#" className="button icon solid fa-file">Learn More</a></li>
+                                </ul>
+                            </article>
 
-            </li>
-        </ul>
-    </section>
+                        </li>
+                    </ul>
+                </section>
 
-</div>
-    )}}
+            </div>
+        )
+    }
+}
 
 export default Sidebar
