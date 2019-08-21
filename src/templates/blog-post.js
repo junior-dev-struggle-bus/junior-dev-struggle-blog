96f22f631e0b702769100240918c066c47d84cc4
Nate Spilman <natespilman@Nates-MacBook-Pro.local>
2019-08-20 21:42:33 -0700

    breaking out the sidebar into its own shared component and adding it to both index and blog-post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,7 +3,7 @@ import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Sidebar from "../components/blogpost/Sidebar"
+import Sidebar from "../components/shared/Sidebar"
 import Post from "../components/blogpost/Post"
 
 class BlogPostTemplate extends React.Component {
@@ -11,6 +11,32 @@ class BlogPostTemplate extends React.Component {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
+
+    const boxExerpts = [
+      {
+        title:"juniordevstrugglebus.com",
+        url:"https://juniordevstrugglebus.com",
+        content:"Visit the Junior Dev Struggle Bus website for more information on the organzation, and opportunities to contribute to the community!"
+      },
+      {
+        title:"algorithms-anon.org",
+        url:"https://algorithms-anon.org",
+        content:"Keep your mind sharp with regular community written algorithm challenges. Join the message board and engage in brain teasers or solve for function of Big O. "
+      },
+      {
+        title:"The Slack Channel",
+        url:"http://juniordevstrugglebus.com/slack",
+        content:"Our Slack channel is the community's main platform of communication. In addition, we have community build bot applications, and lots of development opportunity."
+
+      },
+      {
+        title:"The Meetup Group",
+        url:"https://www.meetup.com/The-Junior-Dev-Struggle-Bus",
+        content:"The Junior Dev Struggle Bus posts regular meetings in Seattle, WA on Meetup.com. Come hang out, learn a few things and teach us a few things."
+
+      }
+    ]
+
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
@@ -31,7 +57,8 @@ class BlogPostTemplate extends React.Component {
           <section id="main">
             <div className="container">
               <div className="row">
-                <Sidebar />
+              <Sidebar boxExerpts={boxExerpts}/>
+
                 {/* <!-- Content --> */}
                 <div id="content" className="col-8 col-12-medium imp-medium">
                   <Post
