1df7afc53ac182c4be6ba3a55362f43be91a89d6
mnus2019 <tontoas2018@gmail.com>
2019-10-06 17:34:49 -0800

    link to GitHub repo on page load

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,24 +1,39 @@
 import React from 'react'
-import { StaticQuery, Link, graphql } from "gatsby"
-
+import { StaticQuery,graphql } from "gatsby"
+import Img from "gatsby-image"
 import Search from './Search'
 
 const Header = ({ title }) => (
-    <StaticQuery 
-        query={graphql`
-            query SearchIndexQuery {
-                siteSearchIndex {
-                    index
-                }
+    <StaticQuery
+    query={graphql`
+    
+        query SearchIndexQuery {
+            siteSearchIndex {
+                index
             }
-        `}
-        render={data => (
-            <header>
-                <section id="header">
-                    <div className="container">
-                        <h1 id="logo">
-                            <Link to="/">{title}</Link>
-                        </h1>
+            file(relativePath: {eq: "giticon.png"}) {
+                childImageSharp {
+                fixed(width:20,height:20) {
+                    ...GatsbyImageSharpFixed
+                }
+                }
+            
+         }   }
+    `}
+    render={data => (
+        <header>
+            <section id="header">
+                <div className="container">
+                   
+                    <h1 id="logo">
+                        <a href="/">{title}</a> 
+            <a href="https://github.com/junior-dev-struggle-bus/junior-dev-struggle-blog/issues/30"  title="GitHub"> 
+                        <Img
+        fixed={data.file.childImageSharp.fixed}
+        alt="Gatsby Docs are awesome"
+  /> </a>  
+                       
+                    </h1>
                         <Search searchIndex={data.siteSearchIndex.index}/>
                     </div>
                 </section>
